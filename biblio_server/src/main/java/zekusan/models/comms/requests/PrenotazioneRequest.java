226fcfe24cbdffdc17ce2e3d73af468cad2450d1
OSANNnn <145115768+OSANNnn@users.noreply.github.com>
2025-11-25 13:56:58 +0100

    impl class Prenotazione Req

--- biblio_server/src/main/java/zekusan/models/comms/requests/PrenotazioneRequest.java
+++ biblio_server/src/main/java/zekusan/models/comms/requests/PrenotazioneRequest.java
@@ -3,6 +3,23 @@ package zekusan.models.comms.requests;
 import zekusan.models.ItemType;
 
 public class PrenotazioneRequest extends Request {
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public ItemType getType() {
+		return type;
+	}
+
+	public void setType(ItemType type) {
+		this.type = type;
+	}
+
 	private int itemId;
 	private ItemType type;
 }
